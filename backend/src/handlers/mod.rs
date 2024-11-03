f64359388ad878975b13021a512371899ffc2858
matt <mattschwartz@users.noreply.github.com>
2024-11-03 11:27:14 -0600

    .

--- backend/src/handlers/mod.rs
+++ backend/src/handlers/mod.rs
@@ -1,6 +1,1 @@
 pub mod users;
-
-// deprecate these
-pub mod ability;
-pub mod kitty;
-pub mod profile;
--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -1,13 +1,9 @@
-use crate::handlers::{kitty, profile, users};
-use axum::{routing::get, routing::post, Extension, Router};
+use crate::handlers::users;
+use axum::{routing::post, Extension, Router};
 use sqlx::PgPool;
 
 pub fn create_routes(db_pool: PgPool) -> Router {
     Router::new()
         .route("/users", post(users::create_user))
-        // deprecated
-        .route("/profile", post(profile::create_profile))
-        .route("/kitty", post(kitty::add_kitty))
-        .route("/kitty", get(kitty::list_kitties))
         .layer(Extension(db_pool))
 }
